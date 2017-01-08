99dbba495ad5be945306a609c3efe6da4497433d
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-01-08 16:19:27 +0200

    Initial commit

---
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ViewResult Index() {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greetings = hour > 12 ? "Good Aftrenoon" : "Good morning";
+            return View();
+        }
+        [HttpGet]
+        public ViewResult RsvpForm() {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponce guestResponce)
+        {
+            //Todo: Send email to partyowner
+            return View("Thanks", guestResponce);
+        }
+    }
+}
