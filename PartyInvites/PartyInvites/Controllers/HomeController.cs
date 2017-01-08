0ec86853bf68697352972d101291af8921093fa9
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-01-08 21:12:18 +0200

    Added e-mail sending from GoogleMail

--- PartyInvites/PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
 using PartyInvites.Models;
@@ -23,6 +25,20 @@ namespace PartyInvites.Controllers
         public ViewResult RsvpForm(GuestResponce guestResponce) {
             if (ModelState.IsValid) {
                 //Todo: Send email to partyowner
+
+                try {
+                    var client = new SmtpClient("smtp.gmail.com", 587) {
+                        Credentials = new NetworkCredential("Fullemail", "PWD"),
+                        EnableSsl = true
+                    };
+                    client.Send("FullemailFromWho", guestResponce.Email, "RSVP Notification",
+                        guestResponce.Name + " is " + ((guestResponce.WillAttend ?? false) ? "" : "not")
+                + " attending");
+                } catch (Exception ex)
+                {
+                    var m = ex.Message;
+                }
+
                 return View("Thanks", guestResponce);
             } else {
                 //Form not valid
